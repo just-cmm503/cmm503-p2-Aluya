ce8a2b310f17f1610e76b9a61698346892f49c6b
Justice aluya <justice015@gmail.com>
2017-05-04 16:08:31 +0100

    comit 3

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@
 $admin_menus=array("Create Group","Enrol Students","View Students","View Groups","Group Students");
 $db_type="mysql";
 $db_name="localdb";
-$db_user_name="root";
+$db_user_name="azure";
 $db_user_password="6#vWHD_$"; //6#vWHD_$
 $db_host="localhost";
 $db_port="52543";
