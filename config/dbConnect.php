03838a8cadbabeecc887b531fd0e69da5daf55d5
Justice aluya <justice015@gmail.com>
2017-06-06 03:38:17 +0100

    comit 3

--- config/dbConnect.php
+++ config/dbConnect.php
@@ -7,7 +7,7 @@
  */
 include_once "config.php";
 
-if (strpos($_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']), 'localhost') !== false) {
+if (strpos($_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']), 'localhost') == false) {
     $dsn = $GLOBALS['db_type'] . ":host=" . $GLOBALS['db_host'] . ";dbname=" . $GLOBALS['db_name'] . ";port=" . $GLOBALS['db_port'] . ";charset=" . $GLOBALS['charset'];
     
     $opt = [
