dc144889616127cb575ef3412ff57bd5408e6e73
Justice aluya <justice015@gmail.com>
2017-06-06 04:14:35 +0100

    comit 3

--- config/dbConnect.php
+++ config/dbConnect.php
@@ -27,13 +27,13 @@ if (strpos($_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']), 'localhost')
             continue;
         }
         $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-        //echo $connectstr_dbhost." g1 <br> \r\n";
+        echo $connectstr_dbhost." g1 <br> \r\n";
         $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-        //echo $connectstr_dbname." g2 <br> \r\n";;
+        echo $connectstr_dbname." g2 <br> \r\n";;
         $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-        //echo $connectstr_dbusername." g3 <br> \r\n";;
+        echo $connectstr_dbusername." g3 <br> \r\n";;
         $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-        //echo $connectstr_dbpassword." g4 <br> \r\n";;
+        echo $connectstr_dbpassword." g4 <br> \r\n";;
     }
     $dsn = $GLOBALS['db_type'] . ":host=" . $connectstr_dbhost . ";dbname=" . $connectstr_dbname . ";charset=" . $GLOBALS['charset'];
     
