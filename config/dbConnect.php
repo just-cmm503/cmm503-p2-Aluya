f6fd73b90134dd1bcd724ba6a9d2172a9cf6a95a
Justice aluya <justice015@gmail.com>
2017-06-06 04:26:15 +0100

    comit 3

--- config/dbConnect.php
+++ config/dbConnect.php
@@ -7,10 +7,10 @@
  */
 include_once "config.php";
 
-if (strpos($_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']), 'localhost') == false) {
+if (strpos($_SERVER['SERVER_NAME'], 'localhost') == false) {
     $dsn = $GLOBALS['db_type'] . ":host=" . $GLOBALS['db_host'] . ";dbname=" . $GLOBALS['db_name'] . ";port=" . $GLOBALS['db_port'] . ";charset=" . $GLOBALS['charset'];
     echo "am on local host  <br> \r\n";
-    print json_encode($_SERVER);
+    print json_encode($_SERVER['SERVER_NAME']);
     
     $opt = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
